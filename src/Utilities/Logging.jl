68fdb0deb0d0f8b546e93b80063fb70841866c50
dominusmi <barp.edoardo@gmail.com>
2019-11-19 13:02:29 +0100

    Add more logging options

--- src/Utilities/Logging.jl
+++ src/Utilities/Logging.jl
@@ -1,5 +1,7 @@
 LOGGING_PATH = BASE_PATH * "/Logs"
 JOB_LOG_PATH = LOGGING_PATH * "/jobs.txt"
+BASE_LOG_PATH = LOGGING_PATH * "/log.txt"
+ERROR_LOG_PATH = LOGGING_PATH * "/error.txt"
 
 function LogJobInfo(message; print=true)
     date = Dates.now()
@@ -12,6 +14,30 @@ end
 function LogJobError(message; print=true)
     date = Dates.now()
     open(JOB_LOG_PATH, "a") do f
+           write(f, "[ERROR] $date: " * message * "\n")
+    end;
+    print ? println(message) : nothing
+end
+
+function LogInfo(message; print=true)
+    date = Dates.now()
+    open(BASE_LOG_PATH, "a") do f
+           write(f, "[INFO] $date: " * message * "\n")
+    end;
+    print ? println(message) : nothing
+end
+
+function LogWarn(message; print=true)
+    date = Dates.now()
+    open(BASE_LOG_PATH, "a") do f
+           write(f, "[WARN] $date: " * message * "\n")
+    end;
+    print ? println(message) : nothing
+end
+
+function LogError(message; print=true)
+    date = Dates.now()
+    open(ERROR_LOG_PATH, "a") do f
            write(f, "[ERROR] $date: " * message * "\n")
     end;
     print ? println(message) : nothing
