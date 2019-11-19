ddeb49c28e14d6423b9a1a83fefb60fdcf176ab3
dominusmi <barp.edoardo@gmail.com>
2019-11-19 15:21:52 +0100

    Add daily recommendations portfolio

--- src/Jobs/GetDailyRecomendationsMovingAverage.jl
+++ src/Jobs/GetDailyRecomendationsMovingAverage.jl
@@ -7,10 +7,19 @@ using MySQL
 
 Invester.CheckLoadHistory()
 
-# Update long running portfolio
+# Update daily recommendations
 LogJobInfo("--------------------------------------------")
 LogJobInfo("Running daily recommendations Moving Average")
 pf = Invester.LoadPortfolio("dailyMovingAverage")
+deleteat!(pf.investments, 1:size(pf.investments,1))
+Invester.SimulatePortfolioDecisionMaker(pf, Dates.today() - Dates.Day(1), Dates.today())
+Invester.SavePortfolio(pf, "dailyRecommendation")
+LogJobInfo("Succesfully terminated daily recommendations Moving Average")
+
+# Update long running portfolio
+LogJobInfo("--------------------------------------------")
+LogJobInfo("Running historical Moving Average")
+pf = Invester.LoadPortfolio("dailyMovingAverage")
 Invester.SimulatePortfolioDecisionMaker(pf, Dates.today() - Dates.Day(1), Dates.today())
 Invester.SavePortfolio(pf, "dailyMovingAverage")
-LogJobInfo("Succesfully terminated daily recommendations Moving Average")
+LogJobInfo("Succesfully terminated daily historical Moving Average")
