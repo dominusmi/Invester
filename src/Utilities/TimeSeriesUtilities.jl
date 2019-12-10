73c67f3d0320f3a9a1ed1bed41a997387dbc0057
dominusmi <barp.edoardo@gmail.com>
2019-12-10 10:25:12 +0100

    Remove useless Episode generator

--- src/Utilities/TimeSeriesUtilities.jl
+++ src/Utilities/TimeSeriesUtilities.jl
@@ -1,42 +1,5 @@
 """
     Generates random time series episodes of specified length
-    Returns two arrays, episodes and predictions / future.
-    Both are of shape m x n, where m is the length of each episode,
-    and n the number of episodes.
-"""
-function GenerateTimeSeriesEpisodes(
-    data::Union{Array{<:Number}, DataFrame}, episodeLength::Integer, predictLength::Integer, cols::Integer=1;
-        overlap=false, n_eps = nothing)
-
-    fullEpisodeLength = episodeLength + predictLength
-
-    if n_eps == nothing
-        n_eps = Int(floor(size(data,1)/fullEpisodeLength))-1
-    end
-    interval = size(data,1)-fullEpisodeLength
-
-    episodes = zeros(episodeLength, n_eps)
-    predicts = zeros(predictLength, n_eps)
-
-    for i in 1:n_eps
-        if overlap
-            idx = rand(1:interval)
-        else
-            idx = i * episodeLength
-        end
-
-        eps_begin = idx
-        eps_end = idx+episodeLength-1
-        prd_begin = eps_end+1
-        prd_end = prd_begin+predictLength-1
-        episodes[:,i] = data[eps_begin:eps_end, cols]
-        predicts[:,i] = data[prd_begin:prd_end, cols]
-    end
-    episodes, predicts
-end
-
-"""
-    Generates random time series episodes of specified length
     Returns two matrices, episodes and predictions / future.
     Shape: m x f x n
     m: episode length
